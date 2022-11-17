2ac73910af947b4fb763e534f73a446669d4061c
luis <6010ldaniel@gmail.com>
2022-11-16 21:20:33 -0300

    index.html

---
+++ rt-app/src/components/App.js
@@ -0,0 +1,24 @@
+import React from "react";
+
+function App() {
+    <div>
+        <h1>Nova pratica componentes pai e filhos</h1>
+
+        <nav>
+         <ul>
+            <li>link 1</li>
+            <li>link 2</li>
+            <li>link 3</li>
+         </ul>
+        </nav>
+        <main>
+            
+        </main>
+
+
+
+
+
+    </div>
+}
+export default App
